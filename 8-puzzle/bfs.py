1d50b716edebdf9e9b8ea5920dd14eea06fd0834
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-05-12 00:29:20 +0530

    8 puzzle bfs Fixing new state existence in visited and node states

--- 8-puzzle/bfs.py
+++ 8-puzzle/bfs.py
@@ -2,7 +2,7 @@
 # python <file>.py <initial_state> <final_state>
 # python 8-puzzle/bfs.py 1,2,5,3,4,0,6,7,8 0,1,2,3,4,5,6,7,8
 # For Debugging in Visual Code, Run these commands in Debug Console. sys.argv.append("3,1,2,0,4,5,6,7,8") sys.argv.append("0,1,2,3,4,5,6,7,8")
-import sys
+import sys, time
 class Node:
     def __init__(self, state, depth, step, parent):
         self.state = state
@@ -10,12 +10,12 @@ class Node:
         self.step = step
         self.parent = parent
 
-def getPathtoGoal(node, path_to_goal):
-    if node.step == None:
-        return path_to_goal
-    else:
+def getPathtoGoal(node):
+    path_to_goal = []
+    while node.step:
         path_to_goal.insert(0, node.step)
-        return getPathtoGoal(node.parent, path_to_goal)
+        node = node.parent
+    return path_to_goal
 
 def getSteps(node_state):
     empty_space_index = node_state.index(0)
@@ -43,6 +43,7 @@ def bfs(initial_state, given_goal_state):
     goal_state = list(map(int, given_goal_state.split(',')))
     total_nodes_visited = 0
     nodes_list = []
+    states_list = []
     visited_states = []
     if len(init_state) != len(goal_state) or len(init_state) != 9 or len(goal_state) != 9:
         print("Invalid Input, please enter valid input.")
@@ -58,7 +59,7 @@ def bfs(initial_state, given_goal_state):
                 node = nodes_list.pop(0)
                 visited_states.append(node.state)
                 if node.state == goal_state:
-                    path_to_goal = getPathtoGoal(node, path_to_goal)
+                    path_to_goal = getPathtoGoal(node)
                     return path_to_goal
                 else:
                     steps = getSteps(node.state)
@@ -66,12 +67,15 @@ def bfs(initial_state, given_goal_state):
                         new_state = node.state[:]
                         old, new = node.state.index(0), step[1]
                         new_state[new], new_state[old] = new_state[old], new_state[new]
-                        if new_state not in visited_states:
+                        if new_state not in states_list and new_state not in visited_states:
                             nodes_list.append(Node(new_state, node.depth + 1, step[0], node))
+                            states_list.append(new_state)
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
         print("Please enter start state and goal state")
     else:
+        start = time.time()
         path_to_goal = bfs(sys.argv[1], sys.argv[2])
         print(path_to_goal)
+        print("Total time took - ", time.time() - start)
