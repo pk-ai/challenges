bfc9d74f3b89cd5cd376c7bbea0e223f9ece5aee
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-05-12 19:48:12 +0530

    Fixing performance issues in BFS, moved common code to util, added DFS

--- 8-puzzle/dfs.py
+++ 8-puzzle/dfs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 # python <file>.py <initial_state> <final_state>
-# python 8-puzzle/bfs.py 8,6,4,2,1,3,5,7,0 0,1,2,3,4,5,6,7,8
+# python 8-puzzle/dfs.py 1,2,5,3,4,0,6,7,8 0,1,2,3,4,5,6,7,8
 # For Debugging in Visual Code, Run these commands in Debug Console. sys.argv.append("3,1,2,0,4,5,6,7,8") sys.argv.append("0,1,2,3,4,5,6,7,8")
 import sys, time
 from pktippa.util import Node, getPathtoGoal, getSteps
 
 def bfs(initial_state, given_goal_state):
-    init_state = initial_state.split(',')
-    goal_state = given_goal_state.split(',')
+    init_state = list(map(int, initial_state.split(',')))
+    goal_state = list(map(int, given_goal_state.split(',')))
     total_nodes_visited = 0
     nodes_list = []
-    states_list = set()
-    visited_states = set()
+    states_list = []
+    visited_states = []
     if len(init_state) != len(goal_state) or len(init_state) != 9 or len(goal_state) != 9:
         print("Invalid Input, please enter valid input.")
     else:
-        print("Doing BFS to solve.")
+        print("Doing DFS to solve.")
         first_node = Node(init_state, 0, None, None)
         nodes_list.append(first_node)
         path_to_goal = []
@@ -24,28 +24,31 @@ def bfs(initial_state, given_goal_state):
                 return None
             else:
                 node = nodes_list.pop(0)
-                visited_states.add("".join(node.state))
+                visited_states.append(node.state)
                 if node.state == goal_state:
                     path_to_goal = getPathtoGoal(node)
-                    return path_to_goal, len(nodes_list) + len(visited_states), node.depth
+                    return path_to_goal
                 else:
                     steps = getSteps(node.state)
+                    # The below step is different from the BFS approach\
+                    # steps are reversed so that when adding steps to nodes_list stack,
+                    # it has to be in sequence.
+                    steps.reverse()
                     for step in steps:
                         new_state = node.state[:]
-                        old, new = node.state.index("0"), step[1]
+                        old, new = node.state.index(0), step[1]
                         new_state[new], new_state[old] = new_state[old], new_state[new]
-                        new_state_str = "".join(new_state)
-                        if new_state_str not in visited_states and new_state_str not in states_list:
-                            nodes_list.append(Node(new_state, node.depth + 1, step[0], node))
-                            states_list.add(new_state_str)
+                        if new_state not in states_list and new_state not in visited_states:
+                            # The below step is different from the BFS approach
+                            # DFS follows a stack "Last in First out", so element at the start
+                            nodes_list.insert(0, Node(new_state, node.depth + 1, step[0], node))
+                            states_list.append(new_state)
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
         print("Please enter start state and goal state")
     else:
         start = time.time()
-        path_to_goal, nodes_expanded, search_depth = bfs(sys.argv[1], sys.argv[2])
-        print("Path to goal: ", path_to_goal)
-        print("Nodes Expanded: ", nodes_expanded)
-        print("Search Depth: ", search_depth)
+        path_to_goal = bfs(sys.argv[1], sys.argv[2])
+        print(path_to_goal)
         print("Total time took - ", time.time() - start)
---
+++ 8-puzzle/pktippa/util.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, state, depth, step, parent):
+        self.state = state
+        self.depth = depth
+        self.step = step
+        self.parent = parent
+
+def getPathtoGoal(node):
+    path_to_goal = []
+    while node.step:
+        path_to_goal.insert(0, node.step)
+        node = node.parent
+    return path_to_goal
+
+def getSteps(node_state):
+    empty_space_index = node_state.index("0")
+    if empty_space_index == 0:
+        return [('Down', 3), ('Right', 1)]
+    elif empty_space_index == 1:
+        return [('Down', 4), ('Left', 0), ('Right', 2)]
+    elif empty_space_index == 2:
+        return [('Down', 5), ('Left', 1)]
+    elif empty_space_index == 3:
+        return [('Up', 0), ('Down', 6), ('Right', 4)]
+    elif empty_space_index == 4:
+        return [('Up', 1), ('Down', 7), ('Left', 3), ('Right', 5)]
+    elif empty_space_index == 5:
+        return [('Up', 2), ('Down', 8), ('Left', 4)]
+    elif empty_space_index == 6:
+        return [('Up', 3), ('Right', 7)]
+    elif empty_space_index == 7:
+        return [('Up', 4), ('Left', 6), ('Right', 8)]
+    else:
+        return [('Up', 5), ('Left', 7)]
