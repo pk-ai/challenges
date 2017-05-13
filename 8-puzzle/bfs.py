9d6643ed75e1447bbf1f9de1d0a03adb46a35cd6
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-05-13 12:37:10 +0530

    8 puzzle Fixing code for more performance for BFS and DFS

--- 8-puzzle/bfs.py
+++ 8-puzzle/bfs.py
@@ -11,27 +11,28 @@ def bfs(initial_state, given_goal_state):
     total_nodes_visited = 0
     nodes_list = []
     states_list = set()
-    visited_states = set()
+    visited_states_count = 0
+    max_search_depth = 0
     if len(init_state) != len(goal_state) or len(init_state) != 9 or len(goal_state) != 9:
         print("Invalid Input, please enter valid input.")
     else:
         print("Doing BFS to solve.")
         first_node = Node(init_state, 0, None, None)
         nodes_list.append(first_node)
+        states_list.add("".join(init_state))
         path_to_goal = []
         while True:
             if not nodes_list:
-                return None
+                return path_to_goal, visited_states_count, max_search_depth, 0
             else:
                 # Since BFS has to be implemented in Queue, which is First in First out
                 # So we are retrieving the first element
                 node = nodes_list.pop(0)
-                visited_states.add("".join(node.state))
+                max_search_depth = node.depth if node.depth > max_search_depth else max_search_depth
                 if node.state == goal_state:
                     path_to_goal = getPathtoGoal(node)
                     print("Remaining nodes list ", len(nodes_list))
-                    print("visited_states list ", len(visited_states))
-                    return path_to_goal, len(nodes_list) + len(visited_states), node.depth
+                    return path_to_goal, visited_states_count, max_search_depth, node.depth
                 else:
                     steps = getSteps(node.state)
                     for step in steps:
@@ -39,18 +40,21 @@ def bfs(initial_state, given_goal_state):
                         old, new = node.state.index("0"), step[1]
                         new_state[new], new_state[old] = new_state[old], new_state[new]
                         new_state_str = "".join(new_state)
-                        if new_state_str not in visited_states and new_state_str not in states_list:
+                        if new_state_str not in states_list:
                             # Appending the element at the end, so pop(0) can work as Queue
                             nodes_list.append(Node(new_state, node.depth + 1, step[0], node))
+                            max_search_depth = (node.depth + 1) if (node.depth + 1) > max_search_depth else max_search_depth
                             states_list.add(new_state_str)
+                visited_states_count += 1
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
         print("Please enter start state and goal state")
     else:
         start = time.time()
-        path_to_goal, nodes_expanded, search_depth = bfs(sys.argv[1], sys.argv[2])
+        path_to_goal, nodes_expanded, max_search_depth, node_depth = bfs(sys.argv[1], sys.argv[2])
         print("Path to goal: ", path_to_goal)
-        print("total Nodes Expanded: ", nodes_expanded)
-        print("Search Depth: ", search_depth)
+        print("total Nodes Expanded or visited nodes: ", nodes_expanded)
+        print("Max Search Depth: ", max_search_depth)
+        print("Find Node Depth: ", node_depth)
         print("Total time took - ", time.time() - start)
--- 8-puzzle/dfs.py
+++ 8-puzzle/dfs.py
@@ -11,29 +11,29 @@ def bfs(initial_state, given_goal_state):
     total_nodes_visited = 0
     nodes_list = []
     states_list = set()
-    visited_states = set()
+    max_search_depth = 0
+    visited_states_count = 0
     if len(init_state) != len(goal_state) or len(init_state) != 9 or len(goal_state) != 9:
         print("Invalid Input, please enter valid input.")
     else:
         print("Doing DFS to solve.")
         first_node = Node(init_state, 0, None, None)
         nodes_list.append(first_node)
+        states_list.add("".join(init_state))
         path_to_goal = []
         while True:
             if not nodes_list:
-                return None
+                return path_to_goal, visited_states_count, max_search_depth, 0
             else:
                 node = nodes_list.pop(0)
-                visited_states.add("".join(node.state))
+                max_search_depth = node.depth if node.depth > max_search_depth else max_search_depth
                 if node.state == goal_state:
                     path_to_goal = getPathtoGoal(node)
-                    print("nodes list ", len(nodes_list))
-                    print("visited_states list ", len(visited_states))
-                    print("node depth", node.depth)
-                    return path_to_goal
+                    print("Remaining nodes list ", len(nodes_list))
+                    return path_to_goal, visited_states_count, max_search_depth, node.depth
                 else:
                     steps = getSteps(node.state)
-                    # The below step is different from the BFS approach\
+                    # The below step is different from the BFS approach
                     # steps are reversed so that when adding steps to nodes_list stack,
                     # it has to be in sequence.
                     steps.reverse()
@@ -42,18 +42,23 @@ def bfs(initial_state, given_goal_state):
                         old, new = node.state.index("0"), step[1]
                         new_state[new], new_state[old] = new_state[old], new_state[new]
                         new_state_str = "".join(new_state)
-                        if new_state_str not in states_list and new_state_str not in visited_states:
+                        if new_state_str not in states_list:
                             # The below step is different from the BFS approach
                             # DFS follows a stack "Last in First out", so element at the start
                             nodes_list.insert(0, Node(new_state, node.depth + 1, step[0], node))
-                            states_list.add("".join(new_state))
+                            max_search_depth = (node.depth + 1) if (node.depth + 1) > max_search_depth else max_search_depth
+                            states_list.add(new_state_str)
+                visited_states_count += 1
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
         print("Please enter start state and goal state")
     else:
         start = time.time()
-        path_to_goal = bfs(sys.argv[1], sys.argv[2])
-        # Prints big list of Path, get print the path uncomment and see.
+        path_to_goal, nodes_expanded, max_search_depth, node_depth = bfs(sys.argv[1], sys.argv[2])
+        # Prints big list of Path, to print the path uncomment and see.
         #print("Path to goal : ", path_to_goal)
+        print("total Nodes Expanded or visited nodes: ", nodes_expanded)
+        print("Max Search Depth: ", max_search_depth)
+        print("Find Node Depth: ", node_depth)
         print("Total time took - ", time.time() - start)
