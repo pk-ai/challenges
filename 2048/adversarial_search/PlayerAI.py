2444f4393a984f245517efc9971259b207b35fba
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-07-16 21:42:41 +0530

    2048 added wait till 0.2 secs

--- 2048/adversarial_search/PlayerAI.py
+++ 2048/adversarial_search/PlayerAI.py
@@ -1,8 +1,14 @@
 
 from random import randint
 from BaseAI import BaseAI
- 
+import time
+timeTimlimit  = 0.2
 class PlayerAI(BaseAI):
     def getMove(self, grid):
+        prevTime =  time.clock()
+        currentTime = time.clock()
+        while currentTime - prevTime < timeTimlimit:
+            print("IN WHILE")
+            currentTime = time.clock()
         moves = grid.getAvailableMoves()
         return moves[randint(0, len(moves) - 1)] if moves else None
